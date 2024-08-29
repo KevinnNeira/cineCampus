480bebf038e3fc9516218cddc9595ab8705a5b6e
KevinnNeira <160855637+KevinnNeira@users.noreply.github.com>
2024-08-29 08:00:39 -0500

    feat: :sparkles: Se realizo API para Obtener Detalles de Película
    Se va a realizar su respectiva documentacion

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const {ObjectId} = require ('mongodb')
 let obj = new movie();
 
 let params = {}
-params.filter = { _id: new ObjectId('66d05a83b06d8dfb19429683') };
-    params.project =  {_id: 0, nombre: 1, sinopsis: 1}
+    params.filter = { _id: new ObjectId('66d05a83b06d8dfb19429683') };
+    params.project =  {_id: 0,nombre: 1, sinopsis: 1, estados: 1, duracion: 1}
 
 obj.getAllMoviesByParam(params).then(res =>{
     console.log(res);

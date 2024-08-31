003567e2dd5bc79474481df99d57737dd579045b
KevinnNeira <160855637+KevinnNeira@users.noreply.github.com>
2024-08-31 15:49:07 -0500

    feat: :sparkles: Se realizo  el API para Reservar Asientos

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 let reservation = require('./js/modules/reservas');
 let obj = new reservation();
 
+let pelicula_id = "66d05a83b06d8dfb19429687"; // ID de la película
+let fecha = "1970-01-04T00:00:00.001Z"; // Fecha de la función
+let hora_inicio = "11:00:00"; // Hora de inicio de la función
+let asientosSeleccionados = [20]; // Asientos que se desean reservar
 
 obj.reserveSeats(pelicula_id, fecha, hora_inicio, asientosSeleccionados)
     .then(res => {
--- js/modules/reservas.js
+++ js/modules/reservas.js
@@ -8,6 +8,7 @@ module.exports = class reservas extends connect {
     }
 
     async reserveSeats(pelicula_id, fecha, hora_inicio, asientosSeleccionados){
+        try {
             await this.open();
             this.collectionTicket = this.db.collection("funciones");
             let peliculaObjectId;
@@ -21,6 +22,25 @@ module.exports = class reservas extends connect {
                 fecha: new Date(fecha),
                 hora_inicio: hora_inicio
             });
-            
+            if (!funcion) {
+                return { message: "Función no encontrada." };
+            }
+            let asientosDisponibles = funcion.asientos.filter(asiento =>
+                asientosSeleccionados.includes(asiento.asiento) && asiento.estado === "Libre"
+            );
+            if (asientosDisponibles.length !== asientosSeleccionados.length) {
+                return { message: "Los asientos se encuentran reservados." };
+            }
+            await this.collectionTicket.updateMany(
+                { _id: funcion._id, "asientos.asiento": { $in: asientosSeleccionados } },
+                { $set: { "asientos.$[elem].estado": "Reservado" } },
+                { arrayFilters: [{ "elem.asiento": { $in: asientosSeleccionados } }] }
+            );
+            funcion = await this.collectionTicket.findOne({ _id: funcion._id });
+            return { message: "Compra exitosa", funcion };
+        } catch (error) {
+            console.error("Error al comprar el ticket:", error);
+            throw error;
+        }
     }
 }

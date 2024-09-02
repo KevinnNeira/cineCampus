aae4274a6e6a66616d410b2d584940b6107f3cc1
KevinnNeira <160855637+KevinnNeira@users.noreply.github.com>
2024-09-02 05:56:40 -0500

    chore: :memo: Se elimino  documentacion del app.js

--- app.js
+++ app.js
@@ -6,20 +6,13 @@ params.filter = { _id: new ObjectId('66d05a83b06d8dfb19429683') };
 params.project =  {_id: 0,nombre: 1, sinopsis: 1, estados: 1, duracion: 1}
 
 
-const Reservation = require('./js/modules/reservas'); // Ajusta la ruta según la estructura de tu proyecto
+const Reservation = require('./js/modules/reservas');
 (async () => {
     try {
-        // Crear una instancia de la clase reservas
         let reservation = new Reservation();
-
-        // Define el ID de la película y el número de asiento que deseas cancelar
         const pelicula_id = '66d05a83b06d8dfb19429687'; // Reemplaza con el ID de la película que necesites
-        const asientoNumero = 20; // Reemplaza con el número del asiento que necesites cancelar
-
-        // Llama a la función cancelSeatReservation y pasa los parámetros necesarios
+        const asientoNumero = 20; // Remplaza por el numero de id de asiento
         let result = await reservation.cancelSeatReservation(pelicula_id, asientoNumero);
-
-        // Muestra el resultado en la consola
         console.log(result);
     } catch (error) {
         console.error("Error al cancelar la reserva:", error);

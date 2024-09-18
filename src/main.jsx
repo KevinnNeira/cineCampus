730fe27eb4dae739031a1f0abeff8014b74a4a33
KevinnNeira <160855637+KevinnNeira@users.noreply.github.com>
2024-09-18 08:57:10 -0500

    style: :lipstick: Se le agrego la vista al crud

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './styles/css/Home.css'
 import './styles/css/LogIn.css'
 import './styles/css/SignUp.css'
+import './styles/css/Crud.css'
 
 ReactDom.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../components/Home';
 import LogIn from '../components/LogIn';
 import SignUp from '../components/SignUp';
+import Crud from '../components/Crud';
 
 export const AppRouter = () => {
     return(
@@ -9,6 +10,7 @@ export const AppRouter = () => {
             <Route path='/' element={<Home/>}/>
             <Route path='/logIn' element={<LogIn/>}/>
             <Route path='/signUp' element={<SignUp/>}/>
+            <Route path='/product' element={<Crud/>}/>
         </Routes>
     )
 }
